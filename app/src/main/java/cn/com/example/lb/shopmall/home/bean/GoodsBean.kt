2f38fd59bed1f030b3841ccfc89578f62280ccc8
joker <3123848646@qq.com>
2018-10-13 18:24:58 +0800

    2018-10-13 第二次提交

--- app/src/main/java/cn/com/example/lb/shopmall/home/bean/GoodsBean.kt
+++ app/src/main/java/cn/com/example/lb/shopmall/home/bean/GoodsBean.kt
@@ -2,13 +2,15 @@ package cn.com.example.lb.shopmall.home.bean
 
 import java.io.Serializable
 
-data class GoodsBean(val cover_price: String,
+ data class GoodsBean(val cover_price: String,
                  val figure: String,
                  val name: String,
-                 val product_id: String):Serializable {
+                 val product_id: String,
+                 var number:Int = 1,
+                 var isSelected:Boolean = true
+ ):Serializable {
 
-    var number:Int = 1
-    var isSelected:Boolean = true
+
 
     override fun toString(): String {
         return "GoodsBean(cover_price='$cover_price', figure='$figure', name='$name', product_id='$product_id', number=$number, isSelected=$isSelected)"
